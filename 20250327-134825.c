1a39261cb39ec3ae09502faecab0b3469f8d656e
luisangelGonzagaguzman66 <guzmangonzaga5@gmail.com>
2025-05-08 13:54:35 -0600

    Add files via upload

---
+++ 20250327-134825.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{int j;
+int i;
+int x;
+
+for (i=0; i<5; i++){
+printf("para i: %d \t",i);
+for(j=0; j<3; j++){
+printf("j%d,",j);}
+printf("\n\n");
+}
+
+
+    
+    return 0;
+}
